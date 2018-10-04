bad5163d64eee8ea672cd5e7cd62ee44c6ef3a22
Simon <34112219+simonmrog@users.noreply.github.com>
2018-10-03 22:47:24 -0500

    Add files via upload

---
+++ 1. Life, the Universe, and Everything.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+//The problem consists in reading and printing data until the number 42 appears
+
+int main()
+{
+    int n;
+    while(scanf("%d", &n) and n != 42) printf("%d\n", n);
+    return (0);
+}
