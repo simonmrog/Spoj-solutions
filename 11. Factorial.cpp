9891e97a1c433c7ec91a3ab0a90e7070aaf561df
Simon <34112219+simonmrog@users.noreply.github.com>
2018-10-04 09:38:53 -0500

    Add files via upload

---
+++ 11. Factorial.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int Z(int);
+
+int main(){
+    int t, N;
+    scanf("%d", &t);
+    for (int i=0; i<t; i++){
+        scanf("%d", &N);
+        printf("%d\n", Z(N));
+    }
+    return (0);
+}
+
+int Z(int N){
+    int n = 0, counter = 0;
+    for (int k=5; k<=N; k*=5) //the limit of K is log5(N), its comes from the inequality N/5^k > 1
+        counter = counter + N/k; //the number of fives determines the number of zeros
+    return counter;
+}
